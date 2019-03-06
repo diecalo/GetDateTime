e07c49cfaa28af83e52c069421e70ff2ef2cce35
Diego Carrera <diego.carrera@fieldeas.com>
2019-03-06 13:51:56 +0100

    Added enum with websites: Google and Apple Added print func more verbose

--- ServerDateTime.playground/Contents.swift
+++ ServerDateTime.playground/Contents.swift
@@ -1,8 +1,19 @@
 import UIKit
 
-func getServerTime(completionHandler: @escaping (_ getResDate: Date?) -> Void) {
+// Allowed websites
+enum Websites: String {
+    case Google = "https://www.google.com"
+    case Apple = "https://www.apple.com"
+}
+
+/// Gets the server datetime from the headers of an allowed website using URLSession
+///
+/// - Parameters:
+///   - url: an URL from allowed websites
+///   - completionHandler: An optional date if succeded
+func getServerTime(url: Websites, completionHandler: @escaping (_ getResDate: Date?) -> Void) {
     
-    guard let url = URL(string: "https://www.google.com") else { return }
+    guard let url = URL(string: url.rawValue) else { return }
     let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
         
         if let httpResponse = response as? HTTPURLResponse,
@@ -18,13 +29,28 @@ func getServerTime(completionHandler: @escaping (_ getResDate: Date?) -> Void) {
     task.resume()
 }
 
-getServerTime { (getResDate) -> Void in
-    guard let date = getResDate else { return }
+/// Prints a date from a website
+///
+/// - Parameters:
+///   - date: Date to print
+///   - url: URL from websites
+func printDateTime(date: Date, url: Websites) {
     let dateFormatter = DateFormatter()
     dateFormatter.dateFormat = "dd-MM-yyyy HH:mm:ss"
     dateFormatter.timeZone = TimeZone(identifier: "CET")
     let dateGet = dateFormatter.string(from: date)
-
-    print("Formatted Time: \(dateGet)")
+    
+    print("Formatted Time: \(dateGet) from: \(url.rawValue)")
 }
 
+getServerTime(url: .Google) { (getResDate) -> Void in
+    guard let date = getResDate else { return }
+
+    printDateTime(date: date, url: .Google)
+}
+
+getServerTime(url: .Apple) { (getResDate) -> Void in
+    guard let date = getResDate else { return }
+
+    printDateTime(date: date, url: .Apple)
+}
